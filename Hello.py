6867a86059fea07da60ba89fe9d2058476c61e1a
ZNing <zhn038@163.com>
2018-06-02 14:41:01 +0800

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,11 @@
+print('Hello world!')
+bob = ['bob smith', 42, 30000, 'software']
+sue = ['sue jones', 45, 40000, 'hardware']
+bob[0], sue[2]
+bob[0].split()[-1]
+bob[0].split()[-2]
+for person in people:
+     print (person)
+
+['bob smith', 42, 30000, 'software']
+['sue jones', 45, 50000.0, 'hardware’]
